7a735b2ca430c82036388aca8bcd2e4e75475ebf
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-01-26 22:24:17 +0900

    Better format

--- rplugin/python3/deoplete/sources/jedi.py
+++ rplugin/python3/deoplete/sources/jedi.py
@@ -25,28 +25,33 @@ class Source(Base):
 
     def get_script(self, source=None, column=None):
         # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_dot_after_module
-        # Adds a dot after a module, because a module that is not accessed this way is definitely not the normal case.
-        # However, in VIM this doesn’t work, that’s why it isn’t used at the moment.
+        # Adds a dot after a module, because a module that is not accessed this
+        # way is definitely not the normal case.  However, in VIM this doesn’t
+        # work, that’s why it isn’t used at the moment.
         jedi.settings.add_dot_after_module = True
 
         # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_bracket_after_function
-        # Adds an opening bracket after a function, because that's normal behaviour.
-        # Removed it again, because in VIM that is not very practical.
+        # Adds an opening bracket after a function, because that's normal
+        # behaviour.  Removed it again, because in VIM that is not very
+        # practical.
         jedi.settings.add_bracket_after_function = True
 
         # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.additional_dynamic_modules
-        # Additional modules in which Jedi checks if statements are to be found.
-        # This is practical for IDEs, that want to administrate their modules themselves.
-        jedi.settings.additional_dynamic_modules = \
-            [b.name for b in self.vim.buffers if b.name is not None and b.name.endswith('.py')]
+        # Additional modules in which Jedi checks if statements are to be
+        # found.  This is practical for IDEs, that want to administrate their
+        # modules themselves.
+        jedi.settings.additional_dynamic_modules = [
+            b.name for b in self.vim.buffers
+            if b.name is not None and b.name.endswith('.py')]
 
-        jedi.settings.cache_directory = os.path.join(os.getenv('XDG_CACHE_HOME'), 'jedi')
+        jedi.settings.cache_directory = os.path.join(
+            os.getenv('XDG_CACHE_HOME'), 'jedi')
 
-        # Need?
+        # Needed?
         if source is None:
             source = '\n'.join(self.vim.current.buffer)
         row = self.vim.current.window.cursor[0]
-        # Need?
+        # Needed?
         if column is None:
             column = self.vim.current.window.cursor[1]
         buf_path = self.vim.current.buffer.name
@@ -60,54 +65,53 @@ class Source(Base):
         if findstart == 1:
             # Really good?
             return column
-        else:
-            # jedi-vim style? or simple?
-            # source = '\n'.join(self.vim.current.buffer[:])
-            source = ''
-            for i, line in enumerate(self.vim.current.buffer):
-                if i == row - 1:
-                    source += line[:column] + str(base) + line[column:]
-                else:
-                    source += line
-                source += '\n'
-            column += base
 
-            out = []
-            try:
-                script = self.get_script(source=source, column=column)
-            except:
-                return out
-            completions = script.completions()
+        # jedi-vim style? or simple?
+        # source = '\n'.join(self.vim.current.buffer[:])
+        source = ''
+        for i, line in enumerate(self.vim.current.buffer):
+            if i == row - 1:
+                source += line[:column] + str(base) + line[column:]
+            else:
+                source += line
+            source += '\n'
 
-            for c in completions:
-                word = c.name[:base] + c.complete
-                abbr = c.name
-                kind = c.description
-                info = c.docstring()
-
-                # Format c.docstring(), Add '(' bracket
-                if c.type == 'function' and re.match(c.name, c.docstring()):
-                    word = c.complete + '('
-                    abbr = re.sub('"(|)",|  ', '',
-                                  c.docstring().split("\n\n")[0].replace('\n', ' '))
-                # Remove '.' for type of 'import'
-                # TODO: '.' completion want in code side
-                #       Need to parse 'import' before the current cursor
-                elif c.type == 'module':
-                    word = c.complete.replace('.', '')
-                # Remove '=', Add '.' for name of 'self'
-                elif c.name == 'self':
-                    word = c.complete.replace('=', '') + '.'
-                else:
-                    word = c.complete
-
-                d = dict(word=word,
-                         abbr=abbr,
-                         kind=kind,
-                         info=info,
-                         icase=1,
-                         dup=1
-                         )
-                out.append(d)
-
+        try:
+            script = self.get_script(source=source, column=column)
+        except:
             return out
+        completions = script.completions()
+
+        out = []
+        for c in completions:
+            word = c.name[:base] + c.complete
+            abbr = c.name
+            kind = c.description
+            info = c.docstring()
+
+            # Format c.docstring(), Add '(' bracket
+            if c.type == 'function' and re.match(c.name, c.docstring()):
+                word = c.complete + '('
+                abbr = re.sub('"(|)",|  ', '',
+                              c.docstring().split("\n\n")[0].replace('\n', ' '))
+            # Remove '.' for type of 'import'
+            # TODO: '.' completion want in code side
+            #       Need to parse 'import' before the current cursor
+            elif c.type == 'module':
+                word = c.complete.replace('.', '')
+            # Remove '=', Add '.' for name of 'self'
+            elif c.name == 'self':
+                word = c.complete.replace('=', '') + '.'
+            else:
+                word = c.complete
+
+            out.append(dict(word=str(word),
+                            abbr=str(abbr),
+                            kind=c.description,
+                            info=c.docstring(),
+                            icase=1,
+                            dup=1
+                            ))
+
+        return out
+>>>>>>> Better format
