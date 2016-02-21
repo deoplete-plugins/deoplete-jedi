206bb164e530576e31b105b600ffbcf40a3b4cde
Koichi Shiraishi <k@zchee.io>
2016-02-21 22:22:54 +0900

    Revert get_script. need jedi settings
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- rplugin/python3/deoplete/sources/deoplete_jedi.py
+++ rplugin/python3/deoplete/sources/deoplete_jedi.py
@@ -40,31 +40,6 @@ class Source(Base):
                 self.vim, 'deoplete#sources#jedi#debug#log_file')
             set_debug(logger, os.path.expanduser(log_file))
 
-        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_dot_after_module
-        # Adds a dot after a module, because a module that is not accessed this
-        # way is definitely not the normal case.  However, in VIM this doesn’t
-        # work, that’s why it isn’t used at the moment.
-        jedi.settings.add_dot_after_module = True
-
-        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_bracket_after_function
-        # Adds an opening bracket after a function, because that's normal
-        # behaviour.  Removed it again, because in VIM that is not very
-        # practical.
-        jedi.settings.add_bracket_after_function = True
-
-        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.additional_dynamic_modules
-        # Additional modules in which Jedi checks if statements are to be
-        # found.  This is practical for IDEs, that want to administrate their
-        # modules themselves.
-        jedi.settings.additional_dynamic_modules = [
-            b.name for b in self.vim.buffers
-            if b.name is not None and b.name.endswith('.py')]
-
-        cache_home = os.getenv('XDG_CACHE_HOME')
-        if cache_home is None:
-            cache_home = os.path.expanduser('~/.cache')
-        jedi.settings.cache_directory = os.path.join(cache_home, 'jedi')
-
     def get_complete_position(self, context):
         m = re.search(r'\w*$', context['input'])
         return m.start() if m else -1
@@ -85,7 +60,7 @@ class Source(Base):
 
         try:
             completions = \
-                jedi.Script(
+                self.get_script(
                     source, line, col, buf.name).completions()
         except Exception:
             return []
@@ -124,3 +99,31 @@ class Source(Base):
                             ))
 
         return out
+
+    def get_script(self, source, line, col, buf_path):
+        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_dot_after_module
+        # Adds a dot after a module, because a module that is not accessed this
+        # way is definitely not the normal case.  However, in VIM this doesn’t
+        # work, that’s why it isn’t used at the moment.
+        jedi.settings.add_dot_after_module = True
+
+        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.add_bracket_after_function
+        # Adds an opening bracket after a function, because that's normal
+        # behaviour.  Removed it again, because in VIM that is not very
+        # practical.
+        jedi.settings.add_bracket_after_function = True
+
+        # http://jedi.jedidjah.ch/en/latest/docs/settings.html#jedi.settings.additional_dynamic_modules
+        # Additional modules in which Jedi checks if statements are to be
+        # found.  This is practical for IDEs, that want to administrate their
+        # modules themselves.
+        jedi.settings.additional_dynamic_modules = [
+            b.name for b in self.vim.buffers
+            if b.name is not None and b.name.endswith('.py')]
+
+        cache_home = os.getenv('XDG_CACHE_HOME')
+        if cache_home is None:
+            cache_home = os.path.expanduser('~/.cache')
+        jedi.settings.cache_directory = os.path.join(cache_home, 'jedi')
+
+        return jedi.Script(source, line, col, buf_path)
