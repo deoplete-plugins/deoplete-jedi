7321d419dc5f2e27a210b529ea99f63029938481
Tommy Allen <tommy@esdf.io>
2017-04-11 13:13:09 -0400

    Implement deoplete async refresh

--- rplugin/python3/deoplete/sources/deoplete_jedi/worker.py
+++ rplugin/python3/deoplete/sources/deoplete_jedi/worker.py
@@ -75,9 +75,10 @@ class Worker(threading.Thread):
 
 def start(count, desc_len=0, short_types=False, show_docstring=False,
           debug=False, python_path=None):
-    while count:
+    while count > 0:
         t = Worker(work_queue, comp_queue, desc_len, short_types,
                    show_docstring, debug, python_path)
         workers.append(t)
         t.start()
+        log.debug('Started worker: %r', t)
         count -= 1
